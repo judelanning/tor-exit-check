b59ecbc36c09184061d7b835504b820d4b8462f4
Jude Lanning <judelanning15@gmail.com>
2023-06-29 15:49:25 -0500

    Added support for passing IPs through CLI

--- tor-exit-node-check.py
+++ tor-exit-node-check.py
@@ -1,9 +1,12 @@
 import urllib.request
+import sys
 
 def get_exit_node_ips():
     with urllib.request.urlopen('https://check.torproject.org/torbulkexitlist') as f:
         ips_string = f.read().decode('utf-8')
+
         ips_list = list(ips_string.split("\n"))
+        
         return ips_list
 
 def check_ip(ip):
@@ -14,6 +17,16 @@ def check_ip(ip):
 
 def manual_check():
     ip_to_check = input("Enter the IP address you would like to check: ")
+
     check_ip(ip_to_check)
 
-manual_check()
+def check_for_cli_arguments():
+    ips = sys.argv[1:]
+
+    if ips == []:
+        manual_check()
+    else:
+        for ip in ips:
+            check_ip(ip)
+
+check_for_cli_arguments()
