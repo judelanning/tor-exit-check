19e31ae30b82416a7b3afd364e5351161c3d61f7
Jude Lanning <judelanning15@gmail.com>
2023-06-29 23:53:21 -0500

    Test

--- tor-exit-check.py
+++ tor-exit-check.py
@@ -6,7 +6,7 @@ def get_exit_node_ips():
         ips_string = f.read().decode('utf-8')
 
         ips_list = list(ips_string.split("\n"))
-        
+
         return ips_list
 
 def check_ip(ip):
@@ -15,18 +15,10 @@ def check_ip(ip):
     else:
         print("No, " + ip + " is not a Tor exit node.")
 
-def manual_check():
-    ip_to_check = input("Enter the IP address you would like to check: ")
-
-    check_ip(ip_to_check)
-
 def check_for_cli_arguments():
     ips = sys.argv[1:]
 
-    if ips == []:
-        manual_check()
-    else:
-        for ip in ips:
-            check_ip(ip)
+    for ip in ips:
+        check_ip(ip)
 
 check_for_cli_arguments()
